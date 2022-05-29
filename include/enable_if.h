c3130fdecad9640cbd74768c48832a7f884b0e0c
Davis Willard <dwillard@un-e5.kzn.21-school.ru>
2022-05-29 17:55:38 +0300

    init commit

---
+++ include/enable_if.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   enable_if.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwillard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/29 16:48:22 by dwillard          #+#    #+#             */
+/*   Updated: 2022/05/29 17:00:37 by dwillard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ENABLE_IF_H
+# define ENABLE_IF_H
+
+namespace ft
+{
+	template <bool B, class T = void>
+	struct enable_if {};
+
+	template <class T>
+	struct enable_if<true, T>
+	{
+		typedef T type;
+	};
+}
+#endif
