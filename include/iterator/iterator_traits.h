1d2d1cfb39d379cd40283288b73c032eb9d3e0ef
Davis Willard <dwillard@un-e1.kzn.21-school.ru>
2022-06-17 17:55:53 +0300

    small style fix

--- include/iterator/iterator_traits.h
+++ include/iterator/iterator_traits.h
@@ -6,7 +6,7 @@
 /*   By: dwillard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 18:21:55 by dwillard          #+#    #+#             */
-/*   Updated: 2022/06/04 17:56:08 by dwillard         ###   ########.fr       */
+/*   Updated: 2022/06/17 17:55:16 by dwillard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,24 @@
 namespace ft
 {
 	template <class It>
-		struct iterator_traits
-		{
-			typedef typename It::iterator_category 	iterator_category;
-			typedef typename It::value_type 		value_type;
-			typedef typename It::distance_type 		distance_type;
-			typedef typename It::pointer 			pointer;
-			typedef typename It::reference 			reference;
-		};
+	struct iterator_traits
+	{
+		typedef typename It::iterator_category 	iterator_category;
+		typedef typename It::value_type 		value_type;
+		typedef typename It::distance_type 		distance_type;
+		typedef typename It::pointer 			pointer;
+		typedef typename It::reference 			reference;
+	};
 
 	template <class T>
-		struct iterator_traits<T *>
-		{
-			typedef T							value_type;
-			typedef T*							pointer;
-			typedef T&							reference;
-			typedef ptrdiff_t					difference_type;
-			typedef random_access_iterator_tag	iterator_category;
-		};
+	struct iterator_traits<T *>
+	{
+		typedef T							value_type;
+		typedef T*							pointer;
+		typedef T&							reference;
+		typedef ptrdiff_t					difference_type;
+		typedef random_access_iterator_tag	iterator_category;
+	};
 
 	template <class T>
 	struct iterator_traits<const T *>
