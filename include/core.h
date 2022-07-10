d7f9d97c14c1f490f397184f808102be2db2771b
Davis Willard <dwillard@pr-f3.kzn.21-school.ru>
2022-07-10 16:06:47 +0300

    map done

--- include/core.h
+++ include/core.h
@@ -1,8 +1,12 @@
 #ifndef CORE_H
 # define CORE_H
 
-# include <iostream>
-# include <string>
-
+#include "algorithm/equal.h"
+#include "algorithm/mismatch.h"
+#include "algorithm/lexicographical_compare.h"
+#include "iterator/iterator.h"
+#include "set/set.h"
+#include "map/map.h"
+#include "utils/utils.h"
 
 #endif
--- include/map/map_traits.h
+++ include/map/map_traits.h
@@ -13,4 +13,65 @@
 #ifndef MAP_TRAITS_H
 #define MAP_TRAITS_H
 
+#include "../utils/utils.h"
+#include <functional>
+
+namespace ft
+{
+	template <class Key, class T, class Pred, class Ax>
+	class	Tmap_traits
+	{
+	public:
+		Pred	comp;
+
+		typedef Key						key_type;
+		typedef ft::pair<const Key, T>	value_type;
+		typedef Pred					key_compare;
+		typedef typename Ax::template
+		rebind<value_type>::other	allocator_type;
+
+		Tmap_traits() : comp()
+		{
+		}
+
+		explicit Tmap_traits(Pred Parg) : comp(Parg)
+		{
+		}
+
+		class value_compare:
+				public std::binary_function<value_type, value_type, bool>
+		{
+			friend class Tmap_traits<Key, T, Pred, Ax>;
+
+		protected:
+			key_compare comp;
+
+		public:
+			bool operator()(const value_type &X,
+							const value_type &Y) const
+			{
+				return comp(X.first, Y.first);
+			}
+
+			value_compare(key_compare Pr) : comp(Pr)
+			{
+			}
+		};
+
+		const static Key &GetKey(const value_type &V)
+		{
+			Kfn	ret;
+			return ret(V);
+		}
+
+		struct Kfn
+		{
+			const Key& operator()(const value_type& X) const
+			{
+				return X.first;
+			}
+		};
+	};
+}
+
 #endif
